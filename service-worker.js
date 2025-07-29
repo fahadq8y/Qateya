db06cb26758ee3f1ec3b5dc4f3bbe77b9bed429b
fahadq8y <f5h5dq8y@gmail.com>
2025-07-29 22:09:40 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,29 @@
 
-const CACHE_NAME = 'qatya-cache-v1';
+const CACHE_NAME = 'qatya-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/354/money-bag_1f4b0.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
 
